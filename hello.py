93f9ce45362e12b054458b1eb0741efdacc16dc9
Ken Youens-Clark <kyclark@gmail.com>
2024-11-22 11:29:17 -0700

    Now says hello to a given name
    I did lots of cool stuff

--- hello.py
+++ hello.py
@@ -6,16 +6,12 @@ Purpose: Rock the Casbah
 """
 
 import argparse
-from typing import NamedTuple, TextIO
+from typing import NamedTuple
 
 
 class Args(NamedTuple):
     """ Command-line arguments """
-    positional: str
-    string_arg: str
-    int_arg: int
-    file: TextIO
-    on: bool
+    name: str
 
 
 # --------------------------------------------------
@@ -26,39 +22,13 @@ def get_args() -> Args:
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument('name',
+                        metavar='NAME',
+                        help='Your name')
 
     args = parser.parse_args()
 
-    return Args(args.positional, args.arg, args.int, args.file, args.on)
+    return Args(args.name)
 
 
 # --------------------------------------------------
@@ -66,17 +36,7 @@ def main() -> None:
     """ Make a jazz noise here """
 
     args = get_args()
-    str_arg = args.string_arg
-    int_arg = args.int_arg
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(f'Hello, {args.name}')
 
 
 # --------------------------------------------------
